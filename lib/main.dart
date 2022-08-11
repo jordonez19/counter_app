79a9f5ae6a613ea87dcbbb44cbce6098ca6467ec
Xavi <javier19pedraza@gmail.com>
2022-08-10 22:56:50 -0500

    counter done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:counter_app/screens/home_screen.dart';
+//import 'package:counter_app/screens/home_screen.dart';
+import 'package:counter_app/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomeScreen()
+        home: CounterScreen()
     );
   }
 }
--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,20 +1,24 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-
-  const HomeScreen({Key? key}) : super(key: key); // key is to identified inside the context
-
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key); 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+class _CounterScreenState extends State<CounterScreen> {
+    int counter = 0;
+ // key is to identified inside the context
   @override
   Widget build(Object context) {
-    int counter = 10;
     //arbol de widgets
     TextStyle font30 = const TextStyle(fontSize: 40);
     //TextStyle weight30 = const TextStyle(fontWeight: FontWeight.bold);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("HOME APP")), //
-        elevation: 0, // shadow
+        title: const Center(child: Text("COUNTER APP")), //
+        elevation: 5, // shadow
       ),
       body: Center(
         child: Column(
@@ -30,14 +34,21 @@ class HomeScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your onPressed code here!
-          counter++;
-          print("hola $counter");
+          setState(() {
+              counter++;
+          });
+          //print("hola $counter");
         },
         backgroundColor: const Color.fromARGB(255, 84, 166, 238),
         child: const Icon(Icons.add),
       ),
     );
   }
-
-  widget({required Row child}) {}
 }
+
+
+
+
+
+
+
